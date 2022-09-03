b036108b6ae55ef5be3d8bc985eb432634e69a00
Bikramjit Roy <96054843+Bikramjit-1@users.noreply.github.com>
2022-09-03 14:59:42 +0530

    Initial commit

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Container, Row, Col } from "reactstrap";
+import CommonSection from "../components/UI/common-section/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+
+import "../styles/checkout.css";
+
+const Checkout = () => {
+  const [enterName, setEnterName] = useState("");
+  const [enterEmail, setEnterEmail] = useState("");
+  const [enterNumber, setEnterNumber] = useState("");
+  const [enterCountry, setEnterCountry] = useState("");
+  const [enterCity, setEnterCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+
+  const shippingInfo = [];
+  const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
+  const shippingCost = 30;
+
+  const totalAmount = cartTotalAmount + Number(shippingCost);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const userShippingAddress = {
+      name: enterName,
+      email: enterEmail,
+      phone: enterNumber,
+      country: enterCountry,
+      city: enterCity,
+      postalCode: postalCode
+    };
+
+    shippingInfo.push(userShippingAddress);
+    console.log(shippingInfo);
+  };
+
+  return (
+    <Helmet title="Checkout">
+      <CommonSection title="Checkout" />
+      <section>
+        <Container>
+          <Row>
+            <h6 className="mb-4">Thank you For Your Order !!!</h6>
+
+            <Col lg="4" md="6">
+              <div className="checkout__bill">
+                <h6 className="d-flex align-items-center justify-content-between mb-3">
+                  Subtotal: <span>${cartTotalAmount}</span>
+                </h6>
+                <h6 className="d-flex align-items-center justify-content-between mb-3">
+                  Shipping: <span>${shippingCost}</span>
+                </h6>
+                <div className="checkout__total">
+                  <h5 className="d-flex align-items-center justify-content-between">
+                    Total: <span>${totalAmount}</span>
+                  </h5>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Checkout;
